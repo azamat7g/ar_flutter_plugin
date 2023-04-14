e350cc83df5ce374df9548d5c085fb6c51abc3f6
Azamat <you@example.com>
2023-04-14 09:29:41 +0500

    added SFB format with animation

--- android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
+++ android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
@@ -263,6 +263,8 @@ internal class AndroidARView(
         try {
             onPause()
             onDestroy()
+
+            modelBuilder.stopAllAnimations()
             ArSceneView.destroyAllResources()
         } catch (e: Exception) {
             e.printStackTrace()
@@ -821,6 +823,45 @@ internal class AndroidARView(
                         }
                         .exceptionally { throwable ->
                             // Pass error to session manager (this has to be done on the main thread if this activity)
+                            val mainHandler = Handler(viewContext.mainLooper)
+                            val runnable = Runnable { sessionManagerChannel.invokeMethod("onError", listOf("Unable to load renderable" + dict_node["uri"] as String)) }
+                            mainHandler.post(runnable)
+                            completableFutureSuccess.completeExceptionally(throwable)
+                            null // return null because java expects void return (in java, void has no instance, whereas in Kotlin, this closure returns a Unit which has one instance)
+                        }
+                }
+                4 -> {
+                    val loader: FlutterLoader = FlutterInjector.instance().flutterLoader()
+                    val key: String = loader.getLookupKeyForAsset(dict_node["uri"] as String)
+
+                    modelBuilder.makeNodeSFB(
+                        viewContext,
+                        transformationSystem,
+                        objectManagerChannel,
+                        enablePans,
+                        enableRotation,
+                        dict_node["name"] as String,
+                        key,
+                        dict_node["transformation"] as ArrayList<Double>
+                    )
+                        .thenAccept { node ->
+                            val anchorName: String? = dict_anchor?.get("name") as? String
+                            val anchorType: Int? = dict_anchor?.get("type") as? Int
+                            if (anchorName != null && anchorType != null) {
+                                val anchorNode = arSceneView.scene.findByName(anchorName) as AnchorNode?
+                                if (anchorNode != null) {
+                                    anchorNode.addChild(node)
+                                    completableFutureSuccess.complete(true)
+                                } else {
+                                    completableFutureSuccess.complete(false)
+                                }
+                            } else {
+                                arSceneView.scene.addChild(node)
+                                completableFutureSuccess.complete(true)
+                            }
+                            completableFutureSuccess.complete(false)
+                        }
+                        .exceptionally { throwable ->
                             val mainHandler = Handler(viewContext.mainLooper)
                             val runnable = Runnable { sessionManagerChannel.invokeMethod("onError", listOf("Unable to load renderable" + dict_node["uri"] as String)) }
                             mainHandler.post(runnable)
